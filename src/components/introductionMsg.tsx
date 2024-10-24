c4b6cafb8b395a55be847e2a616f50bd0234ce60
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-10-24 18:42:30 -0400

    STYLE(#20): Styling (#23)

--- src/components/introductionMsg.tsx
+++ src/components/introductionMsg.tsx
@@ -9,12 +9,17 @@ import React from 'react';
 const IntroductionMsg = () => {
   return (
     <div className="App">
-      <h1>
-        <strong>{COOKING_LAB_TITLE}</strong>
-      </h1>
-      <h2><i>{COOKING_LAB_MOTTO}</i></h2>
-      <p>{COOKING_LAB_WELCOME}</p>
-      <p>{COOKING_LAB_OBJECTIVE}</p>
+      <div className="container my-5">
+        <div className="card shadow p-5">
+          <div className="card-body card-body-bg"></div>
+          <h1>
+            <strong>{COOKING_LAB_TITLE}</strong>
+          </h1>
+          <h2><i>{COOKING_LAB_MOTTO}</i></h2>
+          <p>{COOKING_LAB_WELCOME}</p>
+          <p>{COOKING_LAB_OBJECTIVE}</p>
+        </div>
+      </div>
     </div>
   )
 }
