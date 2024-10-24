70a0e4a02240cbfac20eb63e3ee0910ed93ffdb3
Tien Chung <tchung7171@gmail.com>
2024-10-23 22:08:07 -0400

    STYLE (#20): fixed footer

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,26 @@ import Step2 from 'components/step2';
 import Step3 from 'components/step3';
 import Step4 from 'components/step4';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <NavBar /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/recipe" element={<GetRecipe />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/step1" element={<Step1 />} />
-        <Route path="/step2" element={<Step2 />} />
-        <Route path="/step3" element={<Step3 />} />
-        <Route path="/step4" element={<Step4 />} />
-        <Route path="/summary" element={<SummaryPage />} />
-      </Routes>
+    <div className="app-container">
+      <NavBar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/recipe" element={<GetRecipe />} />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/step1" element={<Step1 />} />
+          <Route path="/step2" element={<Step2 />} />
+          <Route path="/step3" element={<Step3 />} />
+          <Route path="/step4" element={<Step4 />} />
+          <Route path="/summary" element={<SummaryPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
