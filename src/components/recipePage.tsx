f828fb170d5c6840ea785e8bd72971b992976a94
Tien Chung <tchung7171@gmail.com>
2024-10-24 21:22:54 -0400

    Added loading state to recipe page.

--- src/components/recipePage.tsx
+++ src/components/recipePage.tsx
@@ -2,45 +2,55 @@ import {
   RECIPE_INGREDIENT,
   RECIPE_LINK
 } from '../i18n/constants';
-import React from 'react';
+import React, { useState } from 'react';
 import RestartButton from './restartButton';
 import { useNavigate } from 'react-router-dom';
 import { RecipeProps } from '../interfaces/recipeInterface';
 
 const RecipePage = ({label, image, ingredients, url}: RecipeProps) => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  
+  if (!label && !image && !ingredients && !url) {
+    setLoading(true);
+  }
+  
   return (
     <div className="container mt-5">
       <RestartButton />
       <div className="card shadow">
         <div className="card-body card-body-bg">
-          <div className="row mb-4">
-            <div className="col-md-8">
-              <h1 className="card-title">{label}</h1>
-              <h3>{RECIPE_INGREDIENT}</h3>
-              <ul className="list-group">
-                {ingredients?.map(ingredient => (
-                  <li className="list-group-item" key={ingredient.text}>{ingredient.text}</li>
-                ))}
-              </ul>
-            </div>
-            <div className="col-md-4 text-center">
-              <img src={image} alt={label} className="img-fluid shadow" />
-            </div>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <i
-              className="bi bi-arrow-left-circle-fill me-3 ms-3"
-              style={{ fontSize: '2rem', cursor: 'pointer' }}
-              onClick={() => navigate('/summary')}
-            />
-            <div className="row">
-              <div className="col">
-                <h3 className="mt-4">{RECIPE_LINK}</h3>
-                <a className="cooking-lab-link" href={url} target="_blank" rel="noreferrer">{url}</a>
+          {loading ? ( <h1>Loading...</h1> ) : (
+            <>
+              <div className="row mb-4">
+                <div className="col-md-8">
+                  <h1 className="card-title">{label}</h1>
+                  <h3>{RECIPE_INGREDIENT}</h3>
+                  <ul className="list-group">
+                    {ingredients?.map(ingredient => (
+                      <li className="list-group-item" key={ingredient.text}>{ingredient.text}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="col-md-4 text-center">
+                  <img src={image} alt={label} className="img-fluid shadow" />
+                </div>
               </div>
-            </div>
-          </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <i
+                  className="bi bi-arrow-left-circle-fill me-3 ms-3"
+                  style={{ fontSize: '2rem', cursor: 'pointer' }}
+                  onClick={() => navigate('/summary')}
+                />
+                <div className="row">
+                  <div className="col">
+                    <h3 className="mt-4">{RECIPE_LINK}</h3>
+                    <a className="cooking-lab-link" href={url} target="_blank" rel="noreferrer">{url}</a>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
