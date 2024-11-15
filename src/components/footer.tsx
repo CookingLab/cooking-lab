96c078d18e45a1627d8bb44947ab7721af5fd6ca
Tien Chung <tchung7171@gmail.com>
2024-11-15 17:30:04 -0500

    Updated footer margins

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,11 +15,11 @@ const Footer = () => {
   return (
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top background-color footer">
       <div className="col-md-4 d-flex flex-column align-items-start">
-        <div className="d-flex align-items-center mb-3">
+        <div className="d-flex align-items-center mb-3 mx-3">
           <img className="bi me-2 text-muted text-decoration-none lh-1" src={logo} width="30" height="24" alt="Cooking Lab Logo"/>
           <span className="footer-copyright" data-testid="footer-copyright">{COPYRIGHT}</span>
         </div>
-        <p className="footer-copyright" data-testid="footer-power">{POWERED_BY}<a className="footer-link" data-testid="footer-power-link" href={POWERED_BY_LINK} target="_blank" rel="noreferrer">{PROVIDER}</a></p>
+        <p className="footer-copyright mx-3" data-testid="footer-power">{POWERED_BY}<a className="footer-link" data-testid="footer-power-link" href={POWERED_BY_LINK} target="_blank" rel="noreferrer">{PROVIDER}</a></p>
       </div>
 
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex mx-3">
