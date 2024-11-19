162c6acf2096b8c9b106a390ceaa6b6e246d665f
Tien Chung <tchung7171@gmail.com>
2024-11-18 20:41:05 -0500

    fix typo

--- src/i18n/constants.ts
+++ src/i18n/constants.ts
@@ -116,6 +116,6 @@ export const RECIPE_GENERATE = 'Generate another recipe';
 
 // PersonalRecipes constants
 export const PERSONAL_RECIPES_TITLE = 'Our Recipes';
-export const TC_RECIPE = 'Tien\'s Recipe';
-export const TM_RECIPE = 'The Minh\'s Recipe';
+export const TC_RECIPE = 'Tien\'s Recipes';
+export const TM_RECIPE = 'The Minh\'s Recipes';
 export const RECIPE_INSTRUCTIONS = 'Instructions';
