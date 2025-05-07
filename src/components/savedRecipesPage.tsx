cb1ac1adbbb9983c2853b5a7f5698be6868b732f
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-05-07 19:01:33 -0400

    Fixed linter

--- src/components/savedRecipesPage.tsx
+++ src/components/savedRecipesPage.tsx
@@ -6,7 +6,7 @@ import { removeSavedRecipe } from '../redux/cookingLabSlice';
 
 const SavedRecipes = () => {
   const savedRecipes = useSelector((state: RootState) => state.cookingLab.savedRecipes || {});
-  let isSavedRecipesEmpty = Object.entries(savedRecipes).length === 0;
+  const isSavedRecipesEmpty = Object.entries(savedRecipes).length === 0;
   const dispatch = useDispatch();
 
   const handleRemoveSavedRecipe = (name: string) => {
@@ -18,31 +18,37 @@ const SavedRecipes = () => {
       <div className="container mt-5" data-aos="fade-right">
         <div className="card shadow mb-5">
           <div className="card-body card-body-bg">
-           <h1 className="text-center"><strong>{SAVED_RECIPES_TITLE}</strong></h1>
-           {!isSavedRecipesEmpty &&<p className="text-center">{SAVED_RECIPES_MSG}</p>}
+            <h1 className="text-center">
+              <strong>{SAVED_RECIPES_TITLE}</strong>
+            </h1>
+            {!isSavedRecipesEmpty && (
+              <p className="text-center">{SAVED_RECIPES_MSG}</p>
+            )}
             <div className="container">
-                {Object.entries(savedRecipes).length === 0 ? (
+              {isSavedRecipesEmpty ? (
                 <p className="text-center">{NO_SAVED_RECIPES}</p>
-                ) : (
+              ) : (
                 <ul className="list-group">
-                    {Object.entries(savedRecipes).map(([name, url]) => (
+                  {Object.entries(savedRecipes).map(([name, url]) => (
                     <li key={name} className="list-group-item">
-                        <a href={url} target="_blank" rel="noopener noreferrer">{name}</a>
-                        <i
+                      <a href={url} target="_blank" rel="noopener noreferrer">
+                        {name}
+                      </a>
+                      <i
                         className="bi bi-trash-fill float-end"
                         style={{ fontSize: '1.5rem', cursor: 'pointer' }}
                         onClick={() => handleRemoveSavedRecipe(name)}
-                        ></i>
+                      ></i>
                     </li>
-                    ))}
+                  ))}
                 </ul>
-                )}
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default SavedRecipes;
